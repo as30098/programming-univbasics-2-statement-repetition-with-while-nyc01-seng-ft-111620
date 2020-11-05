cd45c0a9ab1c5aba91eada013f69f6d1180c5bc5
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-05 01:16:47 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,3 @@
-while 
+# Build a while...do...end loop that counts from 10 down to 1, outputting the current value during each loop. After 1 is displayed, output Happy New Year!. When completed, your code should produce the following:
+count = 10
+while count <= 10
